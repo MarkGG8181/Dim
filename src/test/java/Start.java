18097512a2943b3c20b102aefc7225058a413213
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-04-13 16:17:27 +0300

    Update Start.java

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,11 +1,11 @@
 import java.io.File;
-import java.lang.reflect.Field;
 import java.util.Arrays;
 
 import net.minecraft.client.main.Main;
 
 public class Start {
 	public static void main(String[] args) {
+		System.setProperty("org.lwjgl.librarypath", new File("./natives").getAbsolutePath());
 		Main.main(concat(new String[] { "--version", "mcp", "--accessToken", "0", "--assetsDir", "assets",
 				"--assetIndex", "1.8", "--userProperties", "{}" }, args));
 	}
